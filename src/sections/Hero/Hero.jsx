48c31738227f189a38fc2b339f7d4af8b1e57d9d
Miroszlav <miroszlav.popovics@gmail.com>
2025-09-28 14:06:08 +0300

    certificate+cv

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
           </p>
 
           <div className={s.actions} data-aos="fade-up" data-aos-delay="400">
-            <Button as="a" href={cvHref} target="_blank">
+            <Button as="a" href={cvHref} target="_blank" dowload>
               {t("hero.cta")}
             </Button>
 
